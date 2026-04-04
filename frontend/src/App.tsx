e706f365183f8834f20ccf4d1769af86710853fe
siddh384 <siddhvyas384@gmail.com>
2026-04-04 16:18:01 +0530

    basic setup created for app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-emerald-400">
+        Tailwind is working perfectly!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
